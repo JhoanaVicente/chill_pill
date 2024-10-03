12812d5e5cbd15a201750d5c628066c6865767ff
Jhoana Vicente <jvicentemorales13@gmail.com>
2024-10-03 15:29:13 +0200

    Adding information slides to buttons

--- src/components/ChillPill.svelte
+++ src/components/ChillPill.svelte
@@ -29,29 +29,91 @@
       isFetching = false; // Restablece el estado de fetch
     }
   }
+
+
+  let buttonDescriptions = {
+    juegosClasicos: "Tetris, Snake",
+    desafiosLogica: "Sudokus, Crucigramas",
+    ejerciciosCreativos: "Dibujar, Colorear",
+    proyectosDIY: "Manualidades, Recetas sencillas",
+  };
+
+  let hoveredDescription = ""; // Para almacenar la descripción al pasar el ratón
+
+
+let showDescription = {
+    juegosClasicos: false,
+    desafiosLogica: false,
+    ejerciciosCreativos: false,
+    proyectosDIY: false,
+  };
+
+  // Función para manejar el clic en cada botón
+  function toggleDescription(buttonName) {
+    showDescription[buttonName] = !showDescription[buttonName];
+  }
 </script>
 
 <section class="chillpill">
   <h1>Chill Pill</h1>
 
-  <div class="container">
-    <button disabled>Juegos Clásicos</button>
-    <button disabled>Desafíos de Lógica</button>
-    <button disabled>Ejercicios Creativos</button>
-    <button disabled>Proyectos DIY</button>
+    <div class="container">
+    <div class="button-container">
+      <button on:click={() => toggleDescription('juegosClasicos')}>
+        Juegos Clásicos
+      </button>
+      {#if showDescription.juegosClasicos}
+        <p class="description-slides">Tetris, Snake</p>
+      {/if}
+    </div>
+
+    <div class="button-container">
+      <button on:click={() => toggleDescription('desafiosLogica')}>
+        Desafíos de Lógica
+      </button>
+      {#if showDescription.desafiosLogica}
+        <p class="description-slides">Sudokus, Crucigramas</p>
+      {/if}
+    </div>
+
+    <div class="button-container">
+      <button on:click={() => toggleDescription('ejerciciosCreativos')}>
+        Ejercicios Creativos
+      </button>
+      {#if showDescription.ejerciciosCreativos}
+        <p class="description-slides">Dibujar, Colorear</p>
+      {/if}
+    </div>
+
+    <div class="button-container">
+      <button on:click={() => toggleDescription('proyectosDIY')}>
+        Proyectos DIY
+      </button>
+      {#if showDescription.proyectosDIY}
+        <p class="description-slides">Manualidades, Recetas sencillas</p>
+      {/if}
+    </div>
   </div>
+    
+    {#if hoveredDescription}
+      <div class="description-popup">
+        <p class="description-slides">{hoveredDescription}</p>
+      </div>
+    {/if}
+    
 
   <div class="container2">
     <img src="../assets/pildora-chillpill.png" alt="Chill Pill" />
     <p>
       ¡Hola! ¿Aburrido? Toma una ChillPill y descubre algo divertido para hacer.
     </p>
-    <button on:click={getActivity}>Sorpréndeme</button>
+    <button class="btn-surprise" on:click={getActivity}>Sorpréndeme</button>
 
     {#if isFetching}
       <p>Cargando actividad...</p>
     {:else if errorMessage}
-      <p style="color: red;">Error: {errorMessage}</p> <!-- Muestra mensaje de error -->
+      <p style="color: red;">Error: {errorMessage}</p>
+      <!-- Muestra mensaje de error -->
     {:else if activity}
       <Activity {activity} /> <!-- Muestra la actividad -->
     {/if}
@@ -93,11 +155,15 @@
     font-size: 18px;
     width: 167px;
     height: 78px;
-    margin: 10px;
     white-space: normal;
     text-align: center;
   }
-
+  
+  .button-container {
+    text-align: center;
+    width: 167px; /* Ajusta el ancho de cada botón */
+    margin: 10px;
+  }
   .container2 {
     text-align: center;
     margin-top: 20px;
@@ -106,7 +172,24 @@
     margin-right: auto;
   }
 
-  .container2 button {
+  .btn-surprise {
+    font-size: 1.5em; /* Aumenta el tamaño de la palabra "Sorpréndeme" */
+    font-weight: bold; /* Puedes hacer el texto más grueso si lo deseas */
+    color: white; /* Color del texto */
+    background-color: #5bb2b0; /* Color del fondo del botón */
+    padding: 20px;
+    border: none;
+    border-radius: 16px;
+    cursor: pointer;
+    text-align: center;
+  }
+
+  .btn-surprise:hover {
+    background-color: #e09c3b; /* Cambia el color de fondo cuando se pasa el ratón */
+    color: #fff; /* Cambia el color del texto al pasar el ratón si lo deseas */
+  }
+
+  button {
     font-family: Lato, sans-serif;
     background-color: #5bb2b0;
     color: white;
@@ -115,10 +198,10 @@
     border-radius: 16px;
     padding: 20px;
     cursor: pointer;
-    font-size: 28px;
-    width: 279px;
-    height: 77px;
-    margin: 10px;
+    font-size: 18px;
+    width: 100%; /* Ancho del botón igual al contenedor */
+    height: 78px;
+    margin: 10px 0;
   }
 
   img {
@@ -134,13 +217,39 @@
     font-weight: bold;
     white-space: normal;
     text-align: center;
-    text-shadow: 1px 0px 5px black, 0px 1px 5px black, 1px 0px 5px black, 0px 1px 5px black;
+    text-shadow:
+      1px 0px 5px black,
+      0px 1px 5px black,
+      1px 0px 5px black,
+      0px 1px 5px black;
     margin: 20px 0;
   }
 
   button:hover {
     background-color: #e09c3b;
   }
+
+  .description-popup {
+  position: absolute; /* Posición absoluta para desplazar la descripción */
+  background-color: #5bb2b0;
+  color: #e09c3b;
+  padding: 10px;
+  border-radius: 5px;
+  z-index: 10; /* Asegúrate de que esté sobre otros elementos */
+  margin-top: 10px; /* Espacio entre el botón y la descripción */
+}
+
+.description-slides {
+    color: #5bb2b0;
+    font-size: 1.5em;
+    font-weight: bold;
+    white-space: normal;
+    text-align: center;
+    margin: 10px 0;
+    padding: 5px;
+    border-radius: 8px;
+  }
+
 
   @media (max-width: 768px) {
     .container {
